64e1736a86534f78f59b791451e3dee86cbdfc39
Rajia Kolleni <rajiaabdulrasheed@gmail.com>
2024-06-08 18:22:52 +0530

    feat: Connect Firebase and implement login/logout functionality
    - Integrated Firebase for authentication and database services
    - Added login and logout functionality with Firebase Auth
    - Implemented loading spinner
    - Integrated react-toastify for error messages

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,54 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup } from '../../firebase'
+import { ColorRing } from 'react-loader-spinner'
 const Login = () => {
-  const [signState, setSignState] = useState("Sign In")
+  const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoding] = useState(false)
+
+  // user authentication
+  const user_auth = async(event)=>{
+    event.preventDefault();
+    setLoding(true);
+      if(signState==="Sign In"){
+        await login(email, password)
+      }else{
+        await signup(name, email, password)
+      }
+     setLoding(false);
+    
+  }
   return (
+    
+    loading?<div className="login-spinner"><ColorRing
+    visible={true}
+    height="60"
+    width="60"
+    ariaLabel="color-ring-loading"
+    wrapperStyle={{}}
+    wrapperClass="color-ring-wrapper"
+    colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+    /></div>:
     <div className='login'>
       <img src={logo} alt="" className="login-logo" />
       <div className="login-form">
-      <form action="">
+      <form onSubmit={user_auth}>
         <h2>{signState}</h2>
-        {signState==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
+        {signState==="Sign Up"?<input value={name} onChange={(e)=>{
+          setName(e.target.value)
+        }}type="text" placeholder='Your name' />:<></>}
         
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Password' />
-        <button>{signState}</button>
+        <input value={email} onChange={(e)=>{
+          setEmail(e.target.value)
+        }} type="email" placeholder='Email' />
+        <input value={password} onChange={(e)=>{
+          setPassword(e.target.value)
+        }} type="password" placeholder='Password' />
+        <button type='submit'>{signState}</button>
         <div className="help">
           <div className="remember">
             <input type="checkbox" name="remember" id="" />
